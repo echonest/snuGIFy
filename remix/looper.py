a9ee97ab8b3198f6179ffc0db44bdcb2c200ee82
Brian Whitman <brian@echonest.com>
2011-11-05 14:15:44 -0400

    uploads to s3, webapp, yay

--- remix/looper.py
+++ remix/looper.py
@@ -11,10 +11,31 @@ import sys
 import tempfile
 import urllib
 import os
+import web
 import echonest.audio as audio
 import pyechonest.song as song
+import echonest.aws as aws
+import random
+import string
+try:
+    import json
+except ImportError:
+    import simplejson as json
 
 
+urls = (
+    '/looper', 'looper'
+)
+app = web.application(urls, globals())
+
+
+
+
+_s3 = aws.connect_s3()
+_bucket = _s3.get_bucket("remix-sounds.sandpit.us")
+
+def random_string(N=8):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(N))
 
 def get_song(combined="kreayshawn gucci gucci"):
     s = song.search(combined=combined,buckets=["id:7digital-US","tracks"],limit=True,results=1)
@@ -40,20 +61,59 @@ def get_loops(fileobj, output_name="out.mp3"):
 
     collect = audio.AudioQuantumList()
     
-    bar = audio_file.analysis.bars[5]
-    for x in xrange(1):
-        collect.append(bar)
+    bars = audio_file.analysis.bars
+    bar_count = 8
+    bar_start = 1
+    repeats = 1
+    if len(bars)-bar_start < bar_count:
+        bar_count = 4
+    if len(bars)-bar_start < bar_count:
+        bar_count = 1
+
+    print "actual bar count was %d" % (bar_count)
+    for y in xrange(repeats):
+        for x in xrange(bar_count):
+            collect.append(audio_file.analysis.bars[bar_start+x])
 
     out = audio.getpieces(audio_file, collect)
-    out.encode(output_name)
-
+    output_temp = tempfile.NamedTemporaryFile(mode="w+b", suffix=".mp3")
+    out.encode(output_temp.name)
+    return output_temp
     
-def main():
+def upload_to_s3(fileobj):
+    fn = random_string()+".mp3"
+    key = _bucket.new_key(fn)
+    key.set_contents_from_file(fileobj)
+    key.set_acl("public-read")
+    key.close()
+    return "http://remix-sounds.sandpit.us/"+fn
+    
+def do_it(search):
     combined = sys.argv[1]
     url = get_song(combined=combined)
     fileobj = download_url(url)
-    get_loops(fileobj)
-
+    oneloopobj = get_loops(fileobj)
+    return upload_to_s3(oneloopobj)
+    
+    
 if __name__ == '__main__':
-    main()
+    print str(do_it(sys.argv[1]))
+
+
+
+class looper:
+    def POST(self):
+        i = web.input(combined=None, bars_count=8, bars_start=1)
+        (url,analysis) = do_it(combined=i.combined, bars_count=i.bars_count, bars_start=i.bars_start)
+        analysis["loop_url"] = url
+        return json.dumps(analysis)
+
+    def GET(self):
+        i = web.input(combined=None, bars_count=8, bars_start=1)
+        (url,analysis) = do_it(combined=i.combined, bars_count=i.bars_count, bars_start=i.bars_start)
+        analysis["loop_url"] = url
+        return json.dumps(analysis)
+
+if __name__ == "__main__":
+    app.run()
 
