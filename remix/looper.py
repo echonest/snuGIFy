5508894c9525019ebbec03ab94e4beb2a0eb8ea3
Brian Whitman <brian@echonest.com>
2011-11-05 13:03:26 -0400

    looper first

---
+++ remix/looper.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+looper.py
+
+Created by Brian Whitman on 2011-11-05.
+Copyright (c) 2011 The Echo Nest. All rights reserved.
+"""
+
+import sys
+import os
+import echonest.audio as audio
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
