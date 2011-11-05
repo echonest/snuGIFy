95aca69abdbd13cf0b0a030ad621ea112abbe33c
Tyler Williams <williams.tyler@gmail.com>
2011-11-05 18:14:12 -0400

    yay before ralph pushes

--- static/js/snugify.js
+++ static/js/snugify.js
@@ -1,3 +1,16 @@
+$.extend({
+    keys:	 function(obj){
+        var a = [];
+        $.each(obj, function(k){ a.push(k); });
+        return a;
+    }
+});
+
+function clear_container(container_name) {
+    var elmt = document.getElementById(container_name);
+    elmt.innerHTML="";
+}
+
 function setupSM() {
     soundManager.url = '/static/';
     soundManager.flashVersion = 9; // optional: shiny features (default = 8)
@@ -21,4 +34,22 @@ function loopFile(url) {
       loops: 100
     });
     return true;
+}
+
+function updateSongInfo(artist, title) {
+    $("#songInfo").html("<p>"+artist+"-"+title+"</p>");
+}
+
+function injectSong(data) {
+    var songObj = JSON.parse(data);
+    updateSongInfo(songObj.artist, songObj.title);
+    loopFile(blob.loop_url);
+}
+
+function searchLoadSong(user_input) {
+    clear_container("songInfo");
+    // looks like this:
+    // http://snuggle.sandpit.us/looper?combined=kreayshawn%20gucci%20gucci
+    $.getJSON("/looper", {combined : user_input}, injectSong);
+    return false;
 }
