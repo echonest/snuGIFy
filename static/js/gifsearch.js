df437d547f8e902bc57af2997de75e94db74f1c8
Ralph Cowling <acastofthousands@gmail.com>
2011-11-05 21:45:46 +0000

    modalll

--- static/js/gifsearch.js
+++ static/js/gifsearch.js
@@ -5,14 +5,20 @@ $(function(){
 		var $this = $(this),
 		//url = $gifSearch.val("action");
 		url = "http://snuggle.sandpit.us/searchgif?query=",
-		finderoo = $("#gifTitle").val();
+		finderoo = $("#gifTitle").val(),
+		$modal = $("#modality"),
+		$modalBody = $modal.find(".modal-body");
 
 		console.log(finderoo);
 		$.ajax({
 			url : url + finderoo,
 			dataType: 'json',
 			success : function(data){
-				console.log(data);
+				populateModal($modalBody,data);
+				$("#modality").modal({
+	                keyboard : true,
+	                show : true
+	            });
 			},
 			error : function (a,b,c) {
 				console.log(a,b,c);
@@ -20,5 +26,13 @@ $(function(){
 		});
 		return false;
 	});
+
+	var populateModal = function($obj,data){
+		var html = "";
+		for (url in data){
+			html += '<img src="'+img.url+'" />';
+		}
+		$body.html(html);
+	};
 
 });
