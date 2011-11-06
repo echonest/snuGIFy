a4a20db8acad4aedbb313c77ae6a8f00d6efdb4f
Brian Whitman <brian@echonest.com>
2011-11-05 20:15:35 -0400

    make it work

--- capsule_support.py
+++ capsule_support.py
@@ -10,7 +10,6 @@ Created by Tristan Jehan and Jason Sundram.
 import numpy as np
 from copy import deepcopy
 from echonest.action import Crossfade, Playback, Crossmatch, Fadein, Fadeout, humanize_time
-from utils import rows, flatten
 
 # constants for now
 X_FADE = 3
@@ -22,6 +21,23 @@ MIN_ALIGN_DURATION = 3
 LOUDNESS_THRESH = -8
 FUSION_INTERVAL = .06   # this is what we use in the analyzer
 AVG_PEAK_OFFSET = 0.025 # Estimated time between onset and peak of segment.
+
+
+def flatten(l):
+    """ Converts a list of tuples to a flat list.
+        e.g. flatten([(1,2), (3,4)]) => [1,2,3,4]
+    """
+    return [item for pair in l for item in pair]
+
+def tuples(l, n=2):
+    """ returns n-tuples from l.
+        e.g. tuples(range(4), n=2) -> [(0, 1), (1, 2), (2, 3)]
+    """
+    return zip(*[l[i:] for i in range(n)])
+
+def rows(m):
+    """returns the # of rows in a numpy matrix"""
+    return m.shape[0]
 
 # TODO: this should probably be in actions?
 def display_actions(actions):
--- main.py
+++ main.py
@@ -22,8 +22,22 @@ from echonest.action import render, make_stereo
 from echonest.audio import LocalAudioFile
 from pyechonest import util
 from capsule_support import resample_features, timbre_whiten, initialize, make_transition, terminate, FADE_OUT, is_valid
-from utils import tuples
 
+def flatten(l):
+    """ Converts a list of tuples to a flat list.
+        e.g. flatten([(1,2), (3,4)]) => [1,2,3,4]
+    """
+    return [item for pair in l for item in pair]
+
+def tuples(l, n=2):
+    """ returns n-tuples from l.
+        e.g. tuples(range(4), n=2) -> [(0, 1), (1, 2), (2, 3)]
+    """
+    return zip(*[l[i:] for i in range(n)])
+
+def rows(m):
+    """returns the # of rows in a numpy matrix"""
+    return m.shape[0]
 
 pconfig.ECHO_NEST_API_KEY = "N6E4NIOVYMTHNDM8J"
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
